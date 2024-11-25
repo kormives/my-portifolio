f06d23e4abb5dae6164b61db91b6e7a7c03eb1e9
Gustavo Tieppo Kormives <gustavotk1710@gmail.com>
2024-11-25 15:49:34 -0300

    feat: configs material ui theme, css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import theme from './theme.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>,
+  );
+} else {
+  console.error('Root element not found.');
+}
